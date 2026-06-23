2cdd379a5dff6a8fc2af9c19a4798b744fad9e0e
thenmozhi-A1 <thenmozhiayothi@gmail.com>
2026-06-23 14:15:38 +0530

    feat: add Workout and Diet Plan entities and APIs

---
+++ gym/gym/src/main/java/com/example/gym/repository/DietPlanRepository.java
@@ -0,0 +1,9 @@
+package com.example.gym.repository;
+
+import com.example.gym.entity.DietPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DietPlanRepository extends JpaRepository<DietPlan, Long> {
+}
