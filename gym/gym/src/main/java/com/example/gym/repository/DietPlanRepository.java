850648f6c004440489529ffe71006f66f6509891
thenmozhi-A1 <thenmozhiayothi@gmail.com>
2026-06-23 15:57:44 +0530

    chore: remove backend from frontend repository

--- gym/gym/src/main/java/com/example/gym/repository/DietPlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gym.repository;
-
-import com.example.gym.entity.DietPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DietPlanRepository extends JpaRepository<DietPlan, Long> {
-}
--- gym/gym/src/main/java/com/example/gym/repository/StaffRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gym.repository;
-
-import com.example.gym.entity.Staff;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface StaffRepository extends JpaRepository<Staff, Long> {
-}
